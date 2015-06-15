7e5250edf7ff15e350d23f92c613bee7536fc12d
Erik Bryn <erik.bryn@gmail.com>
2015-06-15 11:11:35 -0700

    Basic “Hello World” implementation

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
 /* jshint node: true */
 'use strict';
 var Funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-ckeditor',
 
   included: function(app) {
     this._super.included(app);
-    this.bowerDirectory = app.bowerDirectory;
+
     app.import(app.bowerDirectory + '/ckeditor/ckeditor.js');
   },
 
   contentFor: function(type, config) {
-    if (type === 'head') {
-      return "<script>window.CKEDITOR_BASEPATH = '/assets/ckeditor/';</script>";
+    if (type === 'vendor-prefix') {
+      return "window.CKEDITOR_BASEPATH = '/assets/ckeditor/';";
     }
   },
 
-  treeFor: function(type) {
-    var originalTree = this._super.treeFor.call(this, type);
-
-    if (type === 'vendor') {
-      var extraAssets = new Funnel(this.bowerDirectory + '/ckeditor', {
-        srcDir: '/',
-        // include: ['**/*.woff', '**/stylesheet.css'],
-        destDir: '/assets/ckeditor'
-      });
-
-      return mergeTrees([originalTree, extraAssets]);
-    }
-
-    return originalTree;
+  treeForPublic: function(tree) {
+    return new Funnel(this.project.bowerDirectory + '/ckeditor', {
+      srcDir: '/',
+      // include: ['**/*.woff', '**/stylesheet.css'],
+      destDir: '/assets/ckeditor'
+    });
   }
 };
